edc716f9fc859d95d99222c4bbf7b93ab264f62d
Kamel Al-Lababidi <klababidi@winfooz.com>
2019-05-29 17:24:04 +0300

    add splashscreen transition

--- App/Navigators/AppNavigator.js
+++ App/Navigators/AppNavigator.js
@@ -4,6 +4,22 @@ import Login from 'App/Containers/Login/Login'
 import TabNavigator from 'App/Containers/TabNavigator/TabNavigator'
 import Profile from 'App/Containers/Profile/Profile'
 import SplashScreen from 'App/Containers/SplashScreen/SplashScreen'
+import { fadeIn, zoomIn, zoomOut } from 'react-navigation-transitions';
+
+
+
+const handleCustomTransition = ({ scenes }) => {
+  console.log(scenes, "scenes")
+  const prevScene = scenes[scenes.length - 2];
+  const nextScene = scenes[scenes.length - 1];
+
+  if (prevScene
+    && prevScene.route.routeName === 'MainScreen'
+    && nextScene.route.routeName === 'Login') {
+    return fadeIn(2000);
+  } 
+
+}
 
 /**
  * The root screen contains the application's navigation.
@@ -27,6 +43,7 @@ const StackNavigator = createStackNavigator(
     initialRouteName: 'SplashScreen',
     // See https://reactnavigation.org/docs/en/stack-navigator.html#stacknavigatorconfig
     headerMode: 'none',
+    transitionConfig: (nav) => handleCustomTransition(nav)
   }
 )
 
